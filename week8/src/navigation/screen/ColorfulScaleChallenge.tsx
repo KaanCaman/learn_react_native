93c1d49290061f6bce913a2f4f929c77d0a95255
KaanCaman <106434723+KaanCaman@users.noreply.github.com>
2025-05-30 16:33:00 +0300

    Challenge files created.

---
+++ week8/src/navigation/screen/ColorfulScaleChallenge.tsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const ColorfulScaleChallenge = () => {
+  return (
+    <View>
+      <Text>ColorfulScaleChallenge</Text>
+    </View>
+  );
+};
+
+export default ColorfulScaleChallenge;
